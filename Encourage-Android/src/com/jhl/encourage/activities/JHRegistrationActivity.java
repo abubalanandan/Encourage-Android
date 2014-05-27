83d040b1317069518f5622b4d747e898c5f8a949
Abu <abub@qburst.com>
2014-05-28 00:13:23 +0530

    added email and first name validations for registration page

--- Encourage-Android/src/com/jhl/encourage/activities/JHRegistrationActivity.java
+++ Encourage-Android/src/com/jhl/encourage/activities/JHRegistrationActivity.java
@@ -1,22 +1,74 @@
 package com.jhl.encourage.activities;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.jhl.encourage.R;
+import com.jhl.encourage.utilities.JHUtility;
 
 public class JHRegistrationActivity extends Activity {
+
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+	private Pattern pattern;
+	private Matcher matcher;
+	private EditText emailField;
+	private EditText firstNameField;
+	private EditText lastNameField;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.registration);
-
-		
+		pattern = Pattern.compile(EMAIL_PATTERN);
+		initViews();
 	}
 
-	public void loginButtonClicked(View view){
+	private void initViews() {
+		emailField = (EditText) findViewById(R.id.emailField);
+		firstNameField = (EditText) findViewById(R.id.firstNameField);
+		lastNameField = (EditText) findViewById(R.id.lastNameField);
+	}
+
+	public void loginButtonClicked(View view) {
 		finish();
+	}
+
+	private boolean validate(final String hex) {
+
+		matcher = pattern.matcher(hex);
+		return matcher.matches();
+
+	}
+
+	public void registerButtonClicked(View view) {
+		if (firstNameField.getText().toString() == null
+				|| firstNameField.getText().toString().isEmpty()) {
+			JHUtility.showDialogOk("", "Please enter your first name", this);
+			return;
+		}
+		if (emailField.getText().toString() != null
+				&& !emailField.getText().toString().isEmpty()) {
+			if (validate(emailField.getText().toString())) {
+				invokeRegistrationApi(firstNameField.getText().toString(),
+						lastNameField.getText().toString(), emailField
+								.getText().toString());
+			} else {
+				JHUtility.showDialogOk("",
+						"Please enter a valid email address", this);
+			}
+		} else {
+			JHUtility.showDialogOk("", "Please enter your email address", this);
+		}
+	}
+
+	private void invokeRegistrationApi(String firstName, String lastName, String emailAddress) {
+
 	}
 }
