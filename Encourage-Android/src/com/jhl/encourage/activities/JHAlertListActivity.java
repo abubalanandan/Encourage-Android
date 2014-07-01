33ea3a1203759fa029172521f9c8751471209cc0
Abu <abub@qburst.com>
2014-07-01 11:24:28 +0530

    minor change

--- Encourage-Android/src/com/jhl/encourage/activities/JHAlertListActivity.java
+++ Encourage-Android/src/com/jhl/encourage/activities/JHAlertListActivity.java
@@ -16,7 +16,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class JHAlertListActivity extends Activity {
 	ListView listView ;
@@ -32,7 +35,22 @@ public class JHAlertListActivity extends Activity {
         JHAlertsAdapter alertAdapter = new JHAlertsAdapter(this, R.layout.alertitem, alerts);
         ListView alertsList = (ListView)findViewById(R.id.alertListView);
         alertsList.setAdapter(alertAdapter);
-        
+//    	listView.setOnItemClickListener(new OnItemClickListener() {
+//
+//			@Override
+//			public void onItemClick(AdapterView<?> parent, View view,
+//					int position, long id) {
+//				// TODO Auto-generated method stub
+//				if(alerts.get(position).g().equalsIgnoreCase("Map")|| list.get(position).getDatatype().equalsIgnoreCase("Image")){
+//					ImageView imageView = (ImageView)view.findViewWithTag(list.get(position).getTimelineid());
+//					if(imageView!=null){
+//						if(imageView.getBackground().equals(getResources().getDrawable(R.drawable.retry)))
+//						imageLoader.DisplayImage(list.get(position).getFilename(), imageView);
+//					}
+//				}
+//				
+//			}
+//		});
         
     }
     
