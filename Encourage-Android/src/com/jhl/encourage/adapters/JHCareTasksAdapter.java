b60406466f71ed944d05333a8641b711bf5ce818
abub <abub@qburst.com>
2014-07-24 19:42:14 +0200

    Fixed "&nbsp;" in caretask details

--- Encourage-Android/src/com/jhl/encourage/adapters/JHCareTasksAdapter.java
+++ Encourage-Android/src/com/jhl/encourage/adapters/JHCareTasksAdapter.java
@@ -177,6 +177,7 @@ public class JHCareTasksAdapter extends ArrayAdapter<Notification> {
 				pairs[0] = pairs[0].replaceAll("&nbsp;", "");
 				pairs[1] = pairs[1].replaceAll("&nbsp;", "");
 				TableRow.LayoutParams params = new TableRow.LayoutParams(
+						
 						TableRow.LayoutParams.MATCH_PARENT,
 						TableRow.LayoutParams.WRAP_CONTENT);
 				// JHTimelineItemView itemView = new JHTimelineItemView(ctx);
